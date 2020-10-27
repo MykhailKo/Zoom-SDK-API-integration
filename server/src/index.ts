f73d9914627424853964d80dc0b9f431cb07abc6
kostiantynn <kostya.code@gmail.com>
2020-10-27 10:02:52 +0200

    router API | merge with zoom service

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,43 @@
+import express, { Request, Response } from "express";
 (async () => {
+  const app = express();
 
-    const express = require('express');
-    const crypto = require('crypto');
+  const apiRouter = require("./routes/api");
 
-    const apiKey = process.env.API_KEY;
-    const apiSecret = process.env.API_SECRET;
+  app.use("/api", apiRouter);
 
-    let app = express();
-    
-    app.get('/', (_req: any, res: any) => {
-        res.send("Hello world");
-    });
+  app.all("/", (_req: Request, res: Response) => {
+    res.writeHead(301, { Location: "/api" });
+    res.end();
+  });
 
-    app.post('/gensignature', (_req: any, _res: any) => {
-        const timestamp = new Date().getTime() - 30000
-        const msg = Buffer.from(apiKey + _req.body.meetingNumber + timestamp + _req.body.role).toString('base64')
-        const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
-        const signature = Buffer.from(`${apiKey}.${_req.body.meetingNumber}.${timestamp}.${_req.body.role}.${hash}`).toString('base64')
-        _res.status(201).send({signature})
-    });
-
-    app.listen(8000, () => {
-        console.log("Server has started");
-    });
-
+  app.listen(3000, () => console.log("Server has started."));
 })();
+
+// (async () => {
+
+//     const express = require('express');
+//     const crypto = require('crypto');
+
+//     const apiKey = process.env.API_KEY;
+//     const apiSecret = process.env.API_SECRET;
+
+//     let app = express();
+
+//     app.get('/', (_req: any, res: any) => {
+//         res.send("Hello world");
+//     });
+
+//     app.post('/gensignature', (_req: any, _res: any) => {
+//         const timestamp = new Date().getTime() - 30000
+//         const msg = Buffer.from(apiKey + _req.body.meetingNumber + timestamp + _req.body.role).toString('base64')
+//         const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
+//         const signature = Buffer.from(`${apiKey}.${_req.body.meetingNumber}.${timestamp}.${_req.body.role}.${hash}`).toString('base64')
+//         _res.status(201).send({signature})
+//     });
+
+//     app.listen(8000, () => {
+//         console.log("Server has started");
+//     });
+
+// })();
