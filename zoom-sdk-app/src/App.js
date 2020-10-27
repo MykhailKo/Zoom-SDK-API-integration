c9cc1a9a128f9d7c7ac640f59118c19757e7c68f
MykhailKo <tuareg1812@gmail.com>
2020-10-27 23:20:56 +0200

    MED-14 #closed sdk app beta

--- zoom-sdk-app/src/App.js
+++ zoom-sdk-app/src/App.js
@@ -1,34 +1,41 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {ZoomMtg} from "@zoomus/websdk";
 import styles from './App.module.scss';
 import '../node_modules/@zoomus/websdk/dist/css/bootstrap.css';
 import '../node_modules/@zoomus/websdk/dist/css/react-select.css';
 
 const App = () => {
-  const meetConfig = {
-    apiKey: 'Zz0o6PmATEWFOK_DPOCU5A',
-    meetingNumber: '825 0429 3152',
+
+  const [meetConfig, setMeetConfig] = useState({
+    apiKey: 'Zz0o6PmATEWFOK_DPOCU5A', // should be received from the closed endpoint on server
+    meetingNumber: 86928057773,
     leaveUrl: 'localhost:3000',
     userName: 'Mykhail Kosiuk',
     userEmail: 'tuareg1812@gmail.com',
-    passWord: '135462', // if required
+    passWord: '072772', // if required
     role: 1 // 1 for host; 0 for attendee
-  };
+  });
 
   const startMeeting = async () => {
     try {
-    const result = await fetch('http://localhost:8000/gensignature', {
+    const result = await fetch('http://localhost:8000/api/gensignature', {
       method: 'POST',
-      body: JSON.stringify(meetConfig)
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        meetingNumber: meetConfig.meetingNumber,
+        role: meetConfig.role
+      })
     })
-    const response = await result.text();
-    console.log(response);
+    const response = JSON.parse(await result.text());
+    console.log(response.signature);
     ZoomMtg.init({
       leaveUrl: meetConfig.leaveUrl,
       isSupportAV: true,
       success: function() {
         ZoomMtg.join({
-          signature: response,
+          signature: response.signature,
           apiKey: meetConfig.apiKey,
           meetingNumber: meetConfig.meetingNumber,
           userName: meetConfig.userName,
@@ -50,8 +57,16 @@ const App = () => {
 
 
   return (
-    <div>
-      <button onClick={() => startMeeting()} className={styles.startBtn}>Start meeting</button>
+    <div className={styles.zoomHeader}>
+      <input type={"text"} placeholder={"Meeting number"} onChange={(e) => setMeetConfig({...meetConfig, meetingNumber: parseInt(e.target.value)})}/>
+      <input type={"text"} placeholder={"Meeting password"} onChange={(e) => setMeetConfig({...meetConfig, passWord: e.target.value})}/>
+      <input type={"text"} placeholder={"User Name"} onChange={(e) => setMeetConfig({...meetConfig, userName: e.target.value})}/>
+      <input type={"text"} placeholder={"User Email"} onChange={(e) => setMeetConfig({...meetConfig, userEmail: e.target.value})}/>
+      <select onChange={(e) => setMeetConfig({...meetConfig, role: e.target.value})}>
+        <option value={"0"}>Attendee</option>
+        <option value={"1"}>Host</option>
+      </select>
+      <button type={"button"} onClick={() => startMeeting()} className={styles.startBtn}>Start meeting</button>
     </div>
   );
 }
