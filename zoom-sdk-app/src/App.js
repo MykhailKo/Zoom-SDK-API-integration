ce8a90db50fd939f991293826502df538ca873bf
MykhailKo <tuareg1812@gmail.com>
2020-10-27 21:02:07 +0200

    new front

--- zoom-sdk-app/src/App.js
+++ zoom-sdk-app/src/App.js
@@ -1,14 +1,10 @@
-import React, {useRef} from "react";
+import React, {useEffect} from "react";
 import {ZoomMtg} from "@zoomus/websdk";
 import styles from './App.module.scss';
 import '../node_modules/@zoomus/websdk/dist/css/bootstrap.css';
 import '../node_modules/@zoomus/websdk/dist/css/react-select.css';
 
-function App() {
-  ZoomMtg.preLoadWasm();
-  ZoomMtg.prepareJssdk();
-  const zoomRoot = useRef(null);
-
+const App = () => {
   const meetConfig = {
     apiKey: 'Zz0o6PmATEWFOK_DPOCU5A',
     meetingNumber: '825 0429 3152',
@@ -20,11 +16,13 @@ function App() {
   };
 
   const startMeeting = async () => {
-    const result = await fetch('localhost:8000/getsignature', {
+    try {
+    const result = await fetch('http://localhost:8000/gensignature', {
       method: 'POST',
       body: JSON.stringify(meetConfig)
     })
     const response = await result.text();
+    console.log(response);
     ZoomMtg.init({
       leaveUrl: meetConfig.leaveUrl,
       isSupportAV: true,
@@ -39,13 +37,21 @@ function App() {
         })		
       }
     })
+    } catch(error) {
+      console.log(error);
+    }
   }
+
+  useEffect(() => {
+    ZoomMtg.setZoomJSLib('https://source.zoom.us/1.8.1/lib', '/av'); 
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
+  }, []);
+
 
   return (
     <div>
-      <button onClick={() => startMeeting()}>Start meeting</button>
-      <div id="zmmtg-root" ref={zoomRoot}></div>
-      <div id="aria-notify-area"></div>
+      <button onClick={() => startMeeting()} className={styles.startBtn}>Start meeting</button>
     </div>
   );
 }
